ef1a4a47a6ecf1aa2754afd0235186e8abe0fd2f
Ján Kanás <jan.kanas@aardwark.com>
2019-11-28 14:32:52 +0100

    Initial commit

---
+++ src/main/java/com/aardwark/university/bookShop/BookShopApplication.java
@@ -0,0 +1,11 @@
+package com.aardwark.university.bookShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShopApplication {
+    public static void main(String[] args){
+        SpringApplication.run(BookShopApplication.class, args);
+    }
+}
